7fc4187efda0d14a182ac1d6eb7e09b7718023d1
HyunSu1768 <azxcv1768@gmail.com>
2023-10-04 15:28:27 +0900

    hello world

--- src/main/java/com/example/helloworldapp/controller/MainController.java
+++ src/main/java/com/example/helloworldapp/controller/MainController.java
@@ -1,2 +1,18 @@
-package com.example.helloworldapp.controller;public class MainController {
+package com.example.helloworldapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @Autowired
+    Environment env;
+
+    @GetMapping("/")
+    public String main(){
+        return "Hello World!  " + env.getProperty("HOSTNAME");
+    }
 }
